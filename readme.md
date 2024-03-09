b5d73a09022ca31c441284cde471ec5a7ed61e18
Tushar Seth <tusharrealme@gmail.com>
2024-03-09 11:54:23 +0530

    Day 6 commit

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-<h1>ClassAsgn Topics</h1>
-<p>1. How to create a Data Entry Panel<br>2. How to use Panel layout <br> 3. How to use oModel to show the details <br> 4. How to show success and error message on clicking the respective buttons</p>
+<h1>Day 6 Topics</h1>
+<p>1. Create a Simple Form Layout for Employee Details<br>2. Add validations to all the fields of Employee form <br> 3. Create a register button to show the validations are correct or change the colour of input field to red</p>
 
-<h2>Day 4 Preview</h2>
+<h2>Day 6 Preview</h2>
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,20 +9,106 @@ sap.ui.define([
 
         return Controller.extend("com.app.project2.controller.View1", {
             onInit: function () {
-                var oModel=this.getOwnerComponent().getModel("oModel");
-                oModel.setProperty("/data",{});
-                oModel.setProperty("/firstName","Tushar");
-                oModel.setProperty("/lastName","Seth");
-                oModel.setProperty("/post","Software Engineer");
-                console.log(oModel.getData());
 
             },
-            onPressSuccess: function(){
-                sap.m.MessageBox.success(this.getOwnerComponent().getModel("oModel").getProperty("/firstName"));
-            },
-            onPressError : function(){
-                sap.m.MessageBox.error(this.getOwnerComponent().getModel("oModel").getProperty("/lastName"));
-            },
+            onRegister: function(){
+                // alert("Tussu");
+                var empId = this.getView().byId("idIpEmpId").getValue();
+                var empName = this.getView().byId("idIpEmpName").getValue();
+                var empDesig = this.getView().byId("idIpEmpDesig").getValue();
+                var empSkill = this.getView().byId("idIpEmpSkill").getValue();
+                var empEmail = this.getView().byId("idIpEmpEmail").getValue();
+                var empPhone = this.getView().byId("idIpEmpPhone").getValue();
+                // console.log(empId);
+                // this.getOwnerComponent().getModel().setProperty("/empid",empId);
+
+                // Validation for Employee Id
+
+                if(empId === ""){
+                    this.getView().byId("idIpEmpId").setValueState("Error");
+                    this.getView().byId("idIpEmpId").setValueStateText("Employee ID is mandatory please fill it");
+                }
+                else{
+                    this.getView().byId("idIpEmpId").setValueState("None");
+                    var empIdRegExp = /^[0-9]+$/;
+                    if(!empId.match(empIdRegExp)|| empId.length !== 8){
+                            this.getView().byId("idIpEmpId").setValueState("Error");
+                            this.getView().byId("idIpEmpId").setValueStateText("Employee ID should be only digits of length 8");
+                    }
+                }
+                // Validation for Employee Name
+
+                if(empName === ""){
+                    this.getView().byId("idIpEmpName").setValueState("Error");
+                    this.getView().byId("idIpEmpName").setValueStateText("Employee Name is mandatory please fill it");
+                }
+                else{
+                    this.getView().byId("idIpEmpName").setValueState("None");
+                    var empNameRegExp = /^[a-zA-Z ]+$/;
+                    if(!empName.match(empNameRegExp)){
+                            this.getView().byId("idIpEmpName").setValueState("Error");
+                            this.getView().byId("idIpEmpName").setValueStateText("Employee Name should be only be alphabets of length 20");
+                    }
+                }
+                // Validation for Employee Designation
+
+                if(empDesig === ""){
+                    this.getView().byId("idIpEmpDesig").setValueState("Error");
+                    this.getView().byId("idIpEmpDesig").setValueStateText("Employee Designation is mandatory please fill it");
+                }
+                else{
+                    this.getView().byId("idIpEmpDesig").setValueState("None");
+                    var empDesigRegExp = /^[a-zA-Z ]+$/;
+                    if(!empDesig.match(empDesigRegExp)){
+                            this.getView().byId("idIpEmpDesig").setValueState("Error");
+                            this.getView().byId("idIpEmpDesig").setValueStateText("Employee Designation should be only be alphabets");
+                    }
+                }
+                // Validation for Employee Skills
+
+                if(empSkill === ""){
+                    this.getView().byId("idIpEmpSkill").setValueState("Error");
+                    this.getView().byId("idIpEmpSkill").setValueStateText("Employee Skill is mandatory please fill it");
+                }
+                else{
+                    this.getView().byId("idIpEmpSkill").setValueState("None");
+                    var empSkillRegExp = /^[a-zA-Z0-9 ]+$/;
+                    if(!empSkill.match(empSkillRegExp)){
+                            this.getView().byId("idIpEmpSkill").setValueState("Error");
+                            this.getView().byId("idIpEmpSkill").setValueStateText("Employee Skill should be only be alphabets");
+                    }
+                }
+                // Validation for Employee Email
+
+                if(empEmail === ""){
+                    this.getView().byId("idIpEmpEmail").setValueState("Error");
+                    this.getView().byId("idIpEmpEmail").setValueStateText("Employee Email is mandatory please fill it");
+                }
+                else{
+                    this.getView().byId("idIpEmpEmail").setValueState("None");
+                    var empEmailRegExp = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+                    if(!empEmail.match(empEmailRegExp)){
+                            this.getView().byId("idIpEmpEmail").setValueState("Error");
+                            this.getView().byId("idIpEmpEmail").setValueStateText("Employee Email should follow email format");
+                    }
+                }
+                // Validation for Employee Phone
+
+                if(empPhone === ""){
+                    this.getView().byId("idIpEmpPhone").setValueState("Error");
+                    this.getView().byId("idIpEmpPhone").setValueStateText("Employee Phone number is mandatory please fill it");
+                }
+                else{
+                    this.getView().byId("idIpEmpPhone").setValueState("None");
+                    var empPhoneRegExp = /^(?:(?:\+|0{0,2})91(\s*|[\-])?|[0]?)?([6789]\d{2}([ -]?)\d{3}([ -]?)\d{4})$/;
+                    if(!empPhone.match(empPhoneRegExp)){
+                            this.getView().byId("idIpEmpPhone").setValueState("Error");
+                            this.getView().byId("idIpEmpPhone").setValueStateText("Employee Phone number should be of indian format");
+                    }
+                }
+
+
+            }
             // onPress :function(){
             //     var name = this.getView().byId("idIpTxtname").getValue();
             //     alert("Login Succesful✔ "+name);
