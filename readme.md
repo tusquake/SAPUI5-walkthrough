13fa2bb4cc8dc282a6c5f6de6eb9b2ab88dede91
Tushar Seth <tusharrealme@gmail.com>
2024-03-09 09:56:04 +0530

    Day 3 commit

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-This is Walkthrough to the SAP UI5 application
+<h1>Day 3 Topics</h1>
+<p>1. How to horizontally align elements using HBox<br>2. How to Hide and Show elements on screen</p>
+
+<h2>Day 3 Preview</h2>
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -31,6 +31,28 @@ sap.ui.define([
                 //disable input fields
                 this.getView().byId("idIpName").setEnabled(false);
                 // this.getView().byId("idIpAlign").setEnabled(false);
+            },
+            onHideShow: function(){
+                var txtBtn = this.getView().byId("idBtnHideShow").getText();
+
+                if(txtBtn === "Hide"){
+                this.getView().byId("idTxtMsg").setVisible(false);
+                this.getView().byId("idLbName").setVisible(false);
+                this.getView().byId("idIpName").setVisible(false);
+                this.getView().byId("idLbAlign").setVisible(false);
+                this.getView().byId("idIpAlign").setVisible(false);
+
+                this.getView().byId("idBtnHideShow").setText("Show");
+                }
+                else{
+                this.getView().byId("idTxtMsg").setVisible(true);
+                this.getView().byId("idLbName").setVisible(true);
+                this.getView().byId("idIpName").setVisible(true);
+                this.getView().byId("idLbAlign").setVisible(true);
+                this.getView().byId("idIpAlign").setVisible(true);
+
+                this.getView().byId("idBtnHideShow").setText("Hide");
+                }
             }
         });
     });
